7b23d202c660984256698ba62b7afa4ec6095419
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-06-20 15:54:15 -0400

    completed first container

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 //step 1 :define all your html static selectors
 var cityEl = document.querySelector("#city")
 var cityFormEl = document.querySelector("#city-form")
-var cityHeaderEl = document.querySelector("#city-header")
-var cityHeaderCard1El = document.querySelector("#city-header-card-1")
-var cityHeaderCard2El = document.querySelector("#city-header-card-2")
-var cityHeaderCard3El = document.querySelector("#city-header-card-3")
-var cityHeaderCard4El = document.querySelector("#city-header-card-4")
-var cityHeaderCard5El = document.querySelector("#city-header-card-5")
-var TempEl = document.querySelector("#temp")
-var windEl = document.querySelector("#wind")
-var humidityEl = document.querySelector("#humidity")
-var uviEl = document.querySelector("#uvi")
-var temp1El = document.querySelectorAll("temp-1")
-var wind1El = document.querySelector("#wind-1")
-var humidity1El = document.querySelector("humidity-1")
-var uvi1El = document.querySelectorAll("#uvi-1")
-var cityHeaderCard1El = document.querySelector("#city-header-card-1")
+var currentWeatherContainer = document.querySelector('#currentWeather')
+var forecastContainer = document.querySelector('#forecast')
 var api = "43307f36c133c1b4d80feb3644b2ab3e"
 
 var cityNamesEl = document.querySelector("#citynames")
 //step2: make an addEventListener on Submit and create displayDashboard - it shows current weather and last five day
 
-function displayWeather(event) {
+function userInput(event) {
     event.preventDefault()
     var cityName = cityEl.value
-    var urlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}&units=imperial`
+    displayWeather(cityName)
+}
 
+function displayWeather(cityName) {
+    var urlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}&units=imperial`
 
     fetch(urlCurrent)
         .then(function (response) {
@@ -33,34 +23,95 @@ function displayWeather(event) {
         })
         .then(function (currentData) {
 
-            console.log(currentData)
-            var fiveDayUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${api}&units=imperial
+            // console.log(currentData)
+            var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${api}&units=imperial
             `
             fetch(fiveDayUrl)
-            .then(function(response){
-                return response.json()
-            })
-            .then(function(fiveData){
-                console.log(fiveData)
-                var currentDate= moment.unix(currentData.dt).format("MM/DD/YYYY")
-                var iconImage=document.createElement("img")
-                iconImage.setAttribute("src",`http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
-                cityHeaderEl.innerHTML=  currentData.name + " "+ currentDate
-                cityHeaderEl.appendChild(iconImage)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (weatherData) {
+                    // console.log(weatherData)
 
-                
-                TempEl.textContent=currentData.main.temp 
-                 
+                    currentWeather(weatherData.current, currentData.name)
 
-            })
+                    fiveDayforecast(weatherData.daily)
+
+
+                })
         })
 
 
 }
 
+function currentWeather(current, city) {
+    // console.log(current)
+    currentWeatherContainer.innerHTML = '';
+
+    var container = document.createElement('div')
+    var heading = document.createElement('h4')
+    var tempEl = document.createElement('p')
+    var windEl = document.createElement('p')
+    var humidityEl = document.createElement('p')
+    var uviEL = document.createElement('p')
+
+    container.setAttribute('class', 'container-fluid')
+    heading.setAttribute('class', 'display-5 fw-bold')
+    uviEL.setAttribute('Class', 'btn btn-success')
+
+    var currentDate = moment.unix(current.dt).format("MM/DD/YYYY")
+    var iconImage = document.createElement("img")
+    iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`)
+    heading.textContent = city + " " + currentDate
+    heading.append(iconImage)
 
 
-cityFormEl.addEventListener("submit", displayWeather)
+    tempEl.textContent = 'Temp: ' + current.temp
+    windEl.textContent = 'Wind: ' + current.wind_speed
+    humidityEl.textContent = 'Humidity: ' + current.humidity
+    uviEL.textContent = 'UVI: ' + current.uvi
+
+
+    container.append(heading, tempEl, windEl, humidityEl, uviEL)
+    currentWeatherContainer.append(container);
+}
+function fiveDayforecast(forecast) {
+    console.log(forecast)
+    forecastContainer.innerHTML = '';
+
+    for (var i = 1; i < 6; i++) {
+        console.log(i)
+
+        var forecastCol = document.createElement('div')
+        var forecastCard = document.createElement('div')
+        var forecastBody = document.createElement('div')
+        var heading = document.createElement('h4')
+        var tempEl = document.createElement('p')
+        var windEl = document.createElement('p')
+        var humidityEl = document.createElement('p')
+
+        //   add class attributes here for all styling
+
+        forecastCol.setAttribute('class', 'col-sm-2')
+        forecastCard = setAttribute('class', 'card')
+        forecastBody = setAttribute('class', 'card-body')
+        heading = setAttribute('class', 'display-5 fw-bold')
+
+        //   add all the text content for the elements
+        tempEl.textContent = "Wind: " + daily.temp
+        windEl.textContent = "Wind: " + daily.wind_speed
+        humidityEl.textContent = "Wind: " + daily.humidity
+
+
+        // append the elements in the correct order. works in reverse
+
+        container.append(heading,tempEl, windEl, humidityEl)
+        forecastContainer.append(container);
+    }
+}
+
+
+cityFormEl.addEventListener("submit", userInput)
 
 
 
